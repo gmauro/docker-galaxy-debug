a6676feb2017053d6d29954f4b797a5c29f0119a
Gianmauro Cuccuru <gmauro@gmail.com>
2019-04-27 06:53:33 +0200

    add echoes

--- wip/ci.sh
+++ wip/ci.sh
@@ -218,14 +218,21 @@ docker ps
 
 set -e
 
-# Test submitting jobs to an external slurm cluster
+echo "####"
+echo "####"
+echo "# Test submitting jobs to an external slurm cluster"
+echo "####"
+echo "####"
 if [ ! "${COMPOSE_SLURM}" ] && [ ! "${KUBE}" ] && [ ! "${COMPOSE_CONDOR_DOCKER}" ] && [ ! "${COMPOSE_SLURM_SINGULARITY}" ]
 then
     # For compose slurm is already included and thus tested
     cd $TRAVIS_BUILD_DIR/test/slurm/ && bash test.sh && cd $WORKING_DIR
 fi
-
-# Test Web api
+echo "####"
+echo "####"
+echo "# Test Web api"
+echo "####"
+echo "####"
 if [ "${COMPOSE_CONDOR_DOCKER}" ]
 then
     docker-compose logs --tail 50
@@ -242,30 +249,54 @@ curl -v \
      --retry-connrefuse \
      $BIOBLEND_GALAXY_URL/api/version
 
-# Test self-signed HTTPS
+echo "####"
+echo "####"
+echo "# Test self-signed HTTPS"
+echo "####"
+echo "####"
 docker_run -d --name httpstest -p 443:443 -e "USE_HTTPS=True" $DOCKER_RUN_CONTAINER
 # TODO 19.05
 # - sleep 90s && curl -v -k --fail https://127.0.0.1:443/api/version
 #- echo | openssl s_client -connect 127.0.0.1:443 2>/dev/null | openssl x509 -issuer -noout| grep selfsigned
 docker logs httpstest && docker stop httpstest && docker rm httpstest
 
-# Test FTP Server upload
+echo "####"
+echo "####"
+echo "# Test FTP Server upload"
+echo "####"
+echo "####"
  date > time.txt && curl -v --fail -T time.txt ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD || true
 # Test FTP Server get
 curl -v --fail ftp://localhost:8021 --user $GALAXY_USER:$GALAXY_USER_PASSWD
 
-# Test CVMFS
+echo "####"
+echo "####"
+echo "# Test CVMFS"
+echo "####"
+echo "####"
 docker_exec bash -c "service autofs start"
 docker_exec bash -c "cvmfs_config chksetup"
 docker_exec bash -c "ls /cvmfs/data.galaxyproject.org/byhand"
 
-# Test SFTP Server
+echo "####"
+echo "####"
+echo "# Test SFTP Server"
+echo "####"
+echo "####"
 sshpass -p $GALAXY_USER_PASSWD sftp -v -P 8022 -o User=$GALAXY_USER -o "StrictHostKeyChecking no" localhost <<< $'put time.txt'
 
-# Run a ton of BioBlend test against our servers.
+echo "####"
+echo "####"
+echo "# Run a ton of BioBlend test against our servers."
+echo "####"
+echo "####"
 cd $TRAVIS_BUILD_DIR/test/bioblend/ && . ./test.sh && cd $WORKING_DIR/
 
-# Test the 'new' tool installation script
+echo "####"
+echo "####"
+echo "# Test the 'new' tool installation script"
+echo "####"
+echo "####"
 if [ "${COMPOSE_SLURM}" ] || [ "${KUBE}" ] || [ "${COMPOSE_CONDOR_DOCKER}" ] || [ "${COMPOSE_SLURM_SINGULARITY}" ]
 then
     # Compose uses the online installer (uses the running instance)
@@ -275,7 +306,11 @@ else
     docker_exec_run install-tools "$SAMPLE_TOOLS"
 fi
 
-# Test the Conda installation
+echo "####"
+echo "####"
+echo "# Test the Conda installation"
+echo "####"
+echo "####"
 docker_exec_run bash -c 'export PATH=$GALAXY_CONFIG_TOOL_DEPENDENCY_DIR/_conda/bin/:$PATH && conda --version && conda install samtools -c bioconda --yes'
 # Test Docker in Docker, used by Interactive Environments; This needs to be at the end as Docker takes some time to start.
 #- docker_exec docker info
